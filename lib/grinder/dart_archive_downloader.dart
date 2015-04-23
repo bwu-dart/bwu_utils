096776a4f1127dfe3a66a5d4a0ebdc2882135c68
zoechi <gzoechi@gmail.com>
2015-04-23 13:46:22 +0200

    DartArchiveDownloader, load versions and download files

--- lib/grinder/dart_archive_downloader.dart
+++ lib/grinder/dart_archive_downloader.dart
@@ -2,7 +2,9 @@ library bwu_utils.grinder.download_content_shell;
 
 import 'dart:io' as io;
 import 'dart:async' show Completer, Future, Stream;
+import 'dart:convert' show JSON;
 import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
 
 abstract class Channel {
   const Channel();
@@ -52,51 +54,88 @@ class StableChannel extends Channel {
 }
 
 class DartArchiveDownloader {
+  static const host = 'https://gsdview.appspot.com';
   final Channel channel;
 
   DartArchiveDownloader([this.channel = StableChannel.release]);
 
   Stream<Iterable<String>> getVersions([int loadCount = 100]) async* {
-
     int valuesReturned = 0;
     _ParseResult parseResult;
-    Uri nextPage = Uri.parse('http://gsdview.appspot.com/dart-archive/channels/${channel}/');
+    Uri nextPage = buildUrl();
     do {
       final data = await _loadUrl(nextPage);
       parseResult = _parseVersions(data);
       yield parseResult.versions;
 
-      valuesReturned = parseResult.versions.length;
-    }
-    while(valuesReturned < loadCount && parseResult.nextPage != null);
+      valuesReturned += parseResult.versions.length;
+      nextPage = parseResult.nextPage;
+    } while (valuesReturned <= loadCount && nextPage != null);
+  }
 
+  Future<Map> getVersionInfo(String version) async {
+    final content = await _loadUrl(buildUrl(version, 'VERSION'));
+    return JSON.decode(new String.fromCharCodes(content));
+  }
+
+  Future<io.HttpClientRequest> getFileRequest(String version, String file) async {
+    final request = await new io.HttpClient().getUrl(buildUrl(version, file));
+    return request;
+  }
+
+  Future<Null> downloadFile(String version, String file, io.File destination) async {
+    assert(version != null && version.isNotEmpty);
+    assert(file != null && file.isNotEmpty);
+    assert(destination != null && !destination.existsSync());
+    destination.createSync();
+    final fileSink = destination.openWrite();
+    final request =
+        await getFileRequest(version, file);
+    final response = await request.close();
+
+    await fileSink.addStream(response);
   }
 
   static Future<List<int>> _loadUrl(Uri uri) async {
     final doneCompleter = new Completer<List<int>>();
     print(uri);
     final io.HttpClientRequest request = await new io.HttpClient().getUrl(uri);
-      final responseData = <int>[];
-      request.close().then((response) {
-        response.listen((data) {
-          responseData.addAll(data);
-        }, onDone: () {
-          doneCompleter.complete(responseData);
-        });
-      });
+    final responseData = <int>[];
+    final response = await request.close();
+    response.listen((data) {
+      responseData.addAll(data);
+    }, onDone: () {
+      doneCompleter.complete(responseData);
+    });
     return doneCompleter.future;
+  }
 
+  Uri buildUrl([String version, String file]) {
+    assert(channel != null);
+    if (file != null) {
+      assert(version != null);
+      return Uri
+          .parse('${host}/dart-archive/channels/${channel}/${version}/${file}');
+    }
+    if (version != null) {
+      assert(file == null);
+      return Uri.parse('${host}/dart-archive/channels/${channel}/${version}/');
+    }
+    return Uri.parse('${host}/dart-archive/channels/${channel}/');
   }
 
   static _ParseResult _parseVersions(List<int> html) {
-    final Iterable<String> links = parse(html)
+    final Iterable<String> urls = parse(html)
         .querySelectorAll('a[href]')
-        .map((a) =>
-            a.attributes['href'].split('/').where((e) => e.isNotEmpty).last);
-    final versions = links
+        .map((a) => a.attributes['href']);
+    final versions = urls
+        .map((a) => a.split('/').where((e) => e.isNotEmpty).last)
         .where((a) => new RegExp(r'^\d*$').hasMatch(a));
-    final nextPageLinks = links.where((a) => a.startsWith('?marker='));
-    final nextPageLink = nextPageLinks.length == 1 ? Uri.parse(nextPageLinks.first) : null;
+
+    final nextPageLinks = urls.where((a) => a.contains('?marker='));
+    final nextPageLink = nextPageLinks.length == 1
+        ? Uri.parse('${host}${nextPageLinks.first}')
+        : null;
     return new _ParseResult(versions, nextPageLink);
   }
 }
--- test/grinder/dart_archive_downloader_test.dart
+++ test/grinder/dart_archive_downloader_test.dart
@@ -1,12 +1,14 @@
 @TestOn('vm')
 library bwu_utils.test.grinder.dart_archive_downloader;
 
+import 'dart:io' as io;
+import 'package:path/path.dart' as path;
 import 'package:bwu_utils/testing_server.dart';
-import 'package:bwu_utils/grinder/download_content_shell.dart';
-
+import 'package:bwu_utils/grinder/dart_archive_downloader.dart';
 
 void main([List<String> args]) {
   initLogging(args);
+
   group('getVersions', () {
     test('release channel', () async {
       // set up
@@ -14,10 +16,8 @@ void main([List<String> args]) {
 
       // exercise
       final versions = await downloader.getVersions().expand((e) => e).toList();
-      print(versions);
 
       // verification
-      print(versions);
       expect(versions, contains('29803'));
       expect(versions, contains('45104'));
       // tear down
@@ -25,17 +25,127 @@ void main([List<String> args]) {
 
     test('all channels', () async {
       // set up
-      final allChannels = <Channel>[]..addAll(BleedingEdgeChannel.values)..addAll(DevChannel.values)..addAll(StableChannel.values);
-      for(final channel in allChannels) {
+      int maxCount = 0;
+      int minCount = 10000;
+      final allChannels = <Channel>[]
+        ..addAll(BleedingEdgeChannel.values)
+        ..addAll(DevChannel.values)
+        ..addAll(StableChannel.values);
+
+      for (final channel in allChannels) {
         // exercise
         final downloader = new DartArchiveDownloader(channel);
-        final versions = await downloader.getVersions(100).drain();
+        final versions =
+            await downloader.getVersions(2001).expand((e) => e).toList();
 
         // verification
+        if (versions.length > maxCount) {
+          maxCount = versions.length;
+        }
+        if (versions.length < minCount) {
+          minCount = versions.length;
+        }
         expect(versions.length, greaterThan(10));
         expect(versions, everyElement(isNum));
       }
+      expect(maxCount, greaterThan(2000));
+      expect(maxCount, greaterThan(0));
+      print('minCount. $minCount');
+      print('maxCount. $maxCount');
       // tear down
+    }, timeout: const Timeout(const Duration(seconds: 60)), skip: 'slow');
+
+    test('load specific version info', () async {
+      // set up
+
+      // exercise
+      final versionInfo =
+          await new DartArchiveDownloader(BleedingEdgeChannel.raw)
+              .getVersionInfo('45373');
+
+      // verification
+      expect(versionInfo['revision'], '45373');
+      expect(versionInfo['version'], '1.11.0-edge.45373');
+      expect(versionInfo['date'], startsWith('2015042303'));
+
+      // tear down
+    });
+
+    test('load latest version info', () async {
+      // set up
+
+      // exercise
+      final versionInfo = await new DartArchiveDownloader(StableChannel.release)
+          .getVersionInfo('latest');
+
+      // verification
+      expect(versionInfo['revision'].length, greaterThanOrEqualTo(5));
+      expect(versionInfo['version'].length, greaterThanOrEqualTo(5));
+      expect(versionInfo['date'].length, 12);
+      print(versionInfo);
+      // tear down
+    });
+  });
+
+  group('getFileRequest', () {
+    test('load file', () async {
+      // set up
+      const fileName = 'chromedriver-linux-x64-release.zip';
+      // exercise
+      //io.Directory.createTemp()
+      final downloader = new DartArchiveDownloader(BleedingEdgeChannel.raw);
+      io.Directory tempDir;
+      try {
+        tempDir = await io.Directory.systemTemp.createTemp();
+        final io.File destination =
+            await new io.File(path.join(tempDir.absolute.path, fileName))
+                .create();
+        print(destination.absolute.path);
+
+        final fileSink = destination.openWrite();
+
+        final request =
+            await downloader.getFileRequest('latest', 'dartium/${fileName}');
+        final response = await request.close();
+
+        await fileSink.addStream(response);
+
+        // verification
+        final fileSize = await destination.length();
+        expect(fileSize, 2744935);
+      } finally {
+        // tear down
+        if (await tempDir.exists()) {
+          await tempDir.delete(recursive: true);
+        }
+      }
+    });
+  } /*, timeout: const Timeout(const Duration(seconds: 60))*/, skip: 'temporary');
+
+  group('downloadFile', () {
+    test('load file', () async {
+      // set up
+      const fileName = 'chromedriver-linux-x64-release.zip';
+      // exercise
+      //io.Directory.createTemp()
+      final downloader = new DartArchiveDownloader(BleedingEdgeChannel.raw);
+      io.Directory tempDir;
+      try {
+        tempDir = await io.Directory.systemTemp.createTemp();
+        final io.File destination =
+            new io.File(path.join(tempDir.absolute.path, fileName));
+        print(destination.absolute.path);
+        await downloader.downloadFile('latest', 'dartium/${fileName}', destination);
+                // verification
+        final fileSize = await destination.length();
+        expect(fileSize, 2744935);
+
+      } finally {
+        // tear down
+        if (await tempDir.exists()) {
+          await tempDir.delete(recursive: true);
+        }
+      }
     });
   });
 }
