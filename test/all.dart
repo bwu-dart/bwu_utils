ba8c21835d59ee3ee555b11a21642075c70cf71f
zoechi <gzoechi@gmail.com>
2015-07-20 16:20:44 +0200

    add `all.dart` for coveralls Grinder task

--- test/all.dart
+++ test/all.dart
@@ -1,1 +1,11 @@
+library bwu_utils.test.all;
 
+import 'server/network/network_test.dart' as snn;
+import 'server/package/package_test.dart' as spp;
+import 'shared/parse_num_test.dart' as sp;
+
+main() {
+  snn.main();
+  spp.main();
+  sp.main();
+}
